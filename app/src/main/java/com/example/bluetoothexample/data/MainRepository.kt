9ec73d7bd7fed884c33a75d6613e07c1f6ffef4e
VovaDo <v.b.doroshenko@gmail.com>
2022-02-09 16:17:21 +0200

    анализ ответа api запроса

--- app/src/main/java/com/example/bluetoothexample/data/MainRepository.kt
+++ app/src/main/java/com/example/bluetoothexample/data/MainRepository.kt
@@ -6,10 +6,13 @@ import com.example.bluetoothexample.model.BTDevice
 import com.example.bluetoothexample.model.BTScan
 import com.example.bluetoothexample.model.BoundedBTDevicesResponse
 import com.example.bluetoothexample.model.Result
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MainRepository @Inject constructor(private val btdeviceDao: BTDeviceDao) {
@@ -41,12 +44,20 @@ class MainRepository @Inject constructor(private val btdeviceDao: BTDeviceDao) {
         btdeviceDao.deleteList(btDevices)
     }
 
+    @DelicateCoroutinesApi
     suspend fun sendScanData(scans:List<BTScan>){
-        val apiResponse: HashMap<String, Any>? = HttpClient().sendScanData(
-            scans,
-            "mac address"
-        );
-        val error = apiResponse!!["error"] as String?
-        val success = apiResponse["success"] as Int
+        GlobalScope.launch(Dispatchers.IO) {
+            val apiResponse: HashMap<String, Any>? = HttpClient().sendScanData(
+                scans,
+                "mac address"
+            );
+            val error = apiResponse!!["error"] as String?
+            val success = apiResponse["success"] as Int
+
+            if (success == 1){
+                //данные сканирования были успешно переданы на сервер.
+                //на клиенте они больше не нужны и их можно удалить
+            }
+        }
     }
  }
